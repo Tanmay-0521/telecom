aba69477b6b33b992a925ee52145ed0d0872c38b
MrunankW <mann6mann6@gmail.com>
2024-04-16 16:03:14 +0530

    gobar added

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -1,24 +1,39 @@
 /* fonts */
 export const FontFamily = {
-  interBold: "Inter-Bold",
+  b1B: "Inter-Bold",
+  b1: "Inter-Medium",
+  h1: "Inter-ExtraBold",
+  interRegular: "Inter-Regular",
 };
 /* font sizes */
 export const FontSize = {
-  size_31xl: 50,
-  size_6xl: 25,
+  b1_size: 20,
+  h1_size: 40,
+  size_xs: 12,
 };
 /* Colors */
 export const Color = {
-  colorSteelblue: "#6290c8",
-  colorWhite: "#fff",
-  colorDarkslateblue: "#2b50aa",
+  black: "#fff",
+  colorGray: "#7c7c7c",
+  colorBlack: "#000",
+  colorSilver_100: "#c0c0c0",
+  colorSilver_200: "#b7b7b7",
+  colorDarkslategray: "#303030",
+  colorDimgray: "#4d4d4d",
+  colorGainsboro: "#d9d9d9",
 };
 /* Paddings */
 export const Padding = {
+  p_mini: 15,
+  p_sm: 14,
+  p_xs: 12,
   p_3xs: 10,
+  p_6xs: 7,
+  p_2xs: 11,
 };
 /* border radiuses */
 export const Border = {
-  br_43xl: 62,
   br_6xl: 25,
+  br_8xs: 5,
+  br_xl: 20,
 };
