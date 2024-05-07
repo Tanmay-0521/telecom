d2d1127d36e475474a35133f90d765ebae3c7340
MrunankW <mann6mann6@gmail.com>
2024-05-07 17:07:43 +0530

    about,error,warnings

--- screens/About.js
+++ screens/About.js
@@ -1,9 +1,12 @@
 import * as React from "react";
-import { Text, StyleSheet, View, Pressable, ScrollView } from "react-native";
+import { Text, StyleSheet, View, Pressable, ScrollView, SafeAreaView } from "react-native";
 import { Image } from "expo-image";
 import { useNavigation } from "@react-navigation/native";
 import { Color, FontFamily, FontSize, Padding, Border } from "../GlobalStyles";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 //import { ScrollView } from "react-native-gesture-handler";
 
 const About = () => {
@@ -17,21 +20,24 @@ const About = () => {
         contentFit="cover"
         source={require("../assets/logo.png")}
       />
-      {/* <View style = {styles.container}>  */}
-      <ScrollView Vertical={true} contentContainerStyle={styles.scrollViewContent}></ScrollView>
-      <View style={styles.rectangleParent}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView Vertical={true} style={styles.scrollView}>
+          {/* <View style={styles.rectangleParent}>
       
-        <View style={styles.frameChild} />
-        
-        <Text style={styles.lorem}>
-          SysMonitor Pro is a comprehensive system monitoring application
-          designed to provide real-time insights into the critical parameters of
-          your system. With a user-friendly interface and robust functionality,
-          SysMonitor Pro empowers users to keep a close eye on temperature,
-          pressure, voltage, and other essential metrics, ensuring optimal
-          performance and early detection of potential issues .
-        </Text>
-      </View>
+        <View style={styles.frameChild} /> */}
+
+          <Text style={styles.lorem}>
+            SysMonitor Pro is a comprehensive system monitoring application
+            designed to provide real-time insights into the critical parameters
+            of your system. With a user-friendly interface and robust
+            functionality, SysMonitor Pro empowers users to keep a close eye on
+            temperature, pressure, voltage, and other essential metrics,
+            ensuring optimal performance and early detection of potential
+            issues.
+          </Text>
+          {/* </View> */}
+        </ScrollView>
+      </SafeAreaView>
       {/* </View> */}
       <View style={styles.navigator}>
         <View style={styles.navi}>
@@ -82,7 +88,8 @@ const About = () => {
   );
 };
 
-const styles = StyleSheet.create({  //navigation wale (about)
+const styles = StyleSheet.create({
+  //navigation wale (about)
   pageTypo: {
     // marginLeft: 10,
     marginLeft: wp(2),
@@ -93,13 +100,15 @@ const styles = StyleSheet.create({  //navigation wale (about)
     fontSize: wp(6),
     textAlign: "left",
   },
-  frameViewFlexBox: { //navigation wale
+  frameViewFlexBox: {
+    //navigation wale
     // padding: Padding.p_xs,
     padding: wp(2),
     flexDirection: "row",
     alignItems: "center",
   },
-  aboutTheProject: {  //title
+  aboutTheProject: {
+    //title
     // top: 78,
     // left: 39,
     // fontSize: FontSize.h1_size,
@@ -110,13 +119,14 @@ const styles = StyleSheet.create({  //navigation wale (about)
     fontFamily: FontFamily.h1,
     // width: 274,
     // height: 120,
-    width: wp(40),
+    width: wp(50),
     height: hp(20),
     textAlign: "left",
     color: Color.colorBlack,
     position: "absolute",
   },
-  logoIcon: { //logo wala
+  logoIcon: {
+    //logo wala
     // top: 110,
     // right: 37,
     // width: 53,
@@ -127,7 +137,8 @@ const styles = StyleSheet.create({  //navigation wale (about)
     height: hp(5),
     position: "absolute",
   },
-  frameChild: { //about wala
+  frameChild: {
+    //about wala
     alignSelf: "stretch",
     backgroundColor: Color.colorGainsboro,
     // height: 534,
@@ -136,7 +147,8 @@ const styles = StyleSheet.create({  //navigation wale (about)
     zIndex: 0,
     borderRadius: Border.br_xl,
   },
-  lorem: {  //about wala
+  lorem: {
+    //about wala
     // width: "76.87%",
     // top: 16,
     // left: "11.74%",
@@ -145,26 +157,30 @@ const styles = StyleSheet.create({  //navigation wale (about)
     // height: 511,
     zIndex: 1,
     // fontSize: FontSize.b1_size,
-    height: hp(70),
+    height: hp(100),
     top: hp(2),
     left: wp(6),
     width: wp(65),
     fontSize: wp(6),
     textAlign: "left",
     color: Color.colorBlack,
-    position: "absolute",
+    //position: "absolute",
   },
-  rectangleParent: {  //about wale
+  rectangleParent: {
+    //about wale
     width: "78.06%",
-    top: 195,
+    // top: 195,
+    top: hp(20),
     right: "13.06%",
     bottom: 151,
-    left: "8.89%",
+    //left: "8.89%",
+    left: wp(11),
     alignItems: "center",
     borderRadius: Border.br_xl,
     position: "absolute",
   },
-  icon: { //navigation wale
+  icon: {
+    //navigation wale
     borderRadius: Border.br_8xs,
     // width: 30,
     // height: 30,
@@ -184,7 +200,8 @@ const styles = StyleSheet.create({  //navigation wale (about)
     borderRadius: Border.br_6xl,
     flexDirection: "row",
   },
-  navigator: {  //navigation wala
+  navigator: {
+    //navigation wala
     // marginLeft: -144,
     // bottom: 46,
     // left: "50%",
@@ -203,7 +220,23 @@ const styles = StyleSheet.create({  //navigation wale (about)
     height: 800,
     overflow: "hidden",
   },
-  
+  // scrollViewContent: {
+  //   flexGrow: 1,
+  //   justifyContent: 'center',
+  // },
+  scrollView: {
+    backgroundColor: Color.colorGainsboro,
+    marginHorizontal: 20,
+    borderRadius: Border.br_6xl,
+    // top: hp(25),
+    // bottom: hp(50),
+  },
+  container: {
+    flex: 1,
+    paddingBottom: hp(13),
+    paddingTop: hp(23),
+    margin: wp(4),
+  },
 });
 
 export default About;
